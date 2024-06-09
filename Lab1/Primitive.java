c5b333c29cdeb164494876d2ef1a3420b2fb40e0
NikhithaBalla <132688858+NikhithaBalla@users.noreply.github.com>
2024-06-09 17:24:53 +0530

    Add files via upload

---
+++ Lab1/Primitive.java
@@ -0,0 +1,28 @@
+package javafullstack;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		byte age=35;
+	    System.out.println("Age: "+age);
+	    short year=2024;
+	    System.out.println("year: "+year);
+	    int sal=25000;
+	    System.out.println("sal: "+sal);
+	    long pop=9999999l;
+	    System.out.println("pop: "+pop);
+	    float gravity=9.8f;
+	    System.out.println("gravity: "+gravity);
+	    double pi=3.1415;
+	    System.out.println("pi="+pi);
+	    char c='N';
+	    System.out.println("char: "+c);
+	    boolean isMarried=false;
+	    System.out.println("Are you married: "+isMarried);
+	  }
+}
+
+
+	
+
+

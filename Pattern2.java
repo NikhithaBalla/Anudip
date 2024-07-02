5c5196fbd6e7aecb437327bbde570565a3e8048b
NikhithaBalla <132688858+NikhithaBalla@users.noreply.github.com>
2024-07-02 11:02:31 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -3,16 +3,16 @@ package Sample;
 public class Pattern2 {
 
 	public static void main(String[] args) {
-		int n=10;
+        int n=10;
         for(int i=1;i<=n;i++)
-        	 {
+        {
 		for(int j=1;j<=i;j++)
 		{
 			System.out.print("*");
 			
 		}
          System.out.println();
-      }
-	}
+       }
+}
 
 }
